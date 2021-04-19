837db3cdc80a40e4906076e8cd5e29bf1105e7d9
FlintyLemming <admin@flinty.moe>
2021-04-19 22:13:22 +0800

    Initial Commit

---
+++ SilverBlogSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SilverBlogSwift
+//
+//  Created by 沐晨冉 on 2021/4/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SilverBlogSwift/SilverBlogSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverBlogSwiftApp.swift
+//  SilverBlogSwift
+//
+//  Created by 沐晨冉 on 2021/4/19.
+//
+
+import SwiftUI
+
+@main
+struct SilverBlogSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
