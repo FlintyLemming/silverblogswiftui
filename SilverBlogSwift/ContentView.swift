d09fc018604f20eb680fec0957735b59a74e3f93
FlintyLemming <admin@flinty.moe>
2021-04-23 08:47:53 +0800

    First commit

--- SilverBlogSwift/ContentView.swift
+++ SilverBlogSwift/ContentView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+    // 双向绑定
+    @State private var serverAddress = ""
+    @State private var password = ""
+    
+    var invalidInput: Bool{
+        serverAddress.isEmpty || password.isEmpty
     }
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Section {
+                    HStack{
+                        Text("Server")
+                        TextField("Server Address", text: $serverAddress)
+                    }
+                    HStack{
+                        Text("Password")
+                        TextField("Password", text: $password)
+                    }
+                }
+                Section {
+                    Button("Enter") {
+                        
+                    }.disabled(invalidInput)
+                    Button("Use the previous config") {
+                        
+                    }
+                }
+            }
+            .navigationBarTitle("Login")
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
